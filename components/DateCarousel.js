01d0153fb3b8c1afcdc60532fd7eafb07d734197
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2025-03-26 11:34:43 -0500

    feat(DateCarousel): implement calender selection

--- components/DateCarousel.js
+++ components/DateCarousel.js
@@ -7,19 +7,16 @@ export default {
     const TODAYS_DATE = new Date();
 
     let totalConsecutiveClicks = 0;
+    const currentDate = ref(new Date());
+    const showDatePicker = ref(false);
 
     function increment() {
-      totalConsecutiveClicks++
-
-      console.log(totalConsecutiveClicks)
-
+      totalConsecutiveClicks++;
       if (!TIPS_DISABLED && totalConsecutiveClicks >= 10) {
-        console.log('tip you can press the date to open a calender view');
+        console.log('Tip: You can press the date to open a calendar view.');
         totalConsecutiveClicks = 0;
       }
     }
-
-    const currentDate = ref(new Date());
 
     const formatDate = (date) => {
       return date.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
@@ -27,13 +24,17 @@ export default {
 
     const updateDate = (modifier) => {
       const updatedDate = currentDate.value.getDate() + modifier;
-
       if (updatedDate > TODAYS_DATE.getDate()) {
-        console.log('Exiting early we can\'t move to the next day')
+        console.log("Exiting early, we can't move to the next day");
         return;
       }
-
       currentDate.value = new Date(currentDate.value.setDate(currentDate.value.getDate() + modifier));
+      emit("dateChanged", currentDate.value);
+    };
+
+    const selectDate = (date) => {
+      currentDate.value = new Date(date);
+      showDatePicker.value = false;
       emit("dateChanged", currentDate.value);
     };
 
@@ -41,14 +42,24 @@ export default {
       formattedDate: computed(() => formatDate(currentDate.value)),
       prevDate: () => updateDate(-1),
       nextDate: () => updateDate(1),
-      increment
+      increment,
+      showDatePicker,
+      selectDate,
+      currentDate
     };
   },
   template: `<div class="d-flex align-items-center justify-content-center gap-3">
     <button class="btn btn-primary" @click="prevDate(); increment();">&lt;</button>
-    <div class="px-4 py-2 border rounded bg-light">{{ formattedDate }}</div>
+    <div class="px-4 py-2 border rounded bg-light" @click="showDatePicker = true">{{ formattedDate }}</div>
     <button class="btn btn-primary" @click="nextDate">&gt;</button>
-
-    <v-date-picker hidden></v-date-picker>
+    
+    <v-dialog v-model="showDatePicker" persistent>
+      <v-card>
+        <v-date-picker v-model="currentDate" @update:modelValue="selectDate"></v-date-picker>
+        <v-card-actions>
+          <v-btn text @click="showDatePicker = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>`
 }
