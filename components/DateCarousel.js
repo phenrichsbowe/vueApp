01c5007d681891f5185d2f2d543811da41841968
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2025-03-26 11:28:07 -0500

    feat(DateCarousel): add tip for opening calender view

--- components/DateCarousel.js
+++ components/DateCarousel.js
@@ -3,6 +3,22 @@ import { ref, computed } from "vue";
 export default {
   emits: ["dateChanged"],
   setup(_, { emit }) {
+    const TIPS_DISABLED = false;
+    const TODAYS_DATE = new Date();
+
+    let totalConsecutiveClicks = 0;
+
+    function increment() {
+      totalConsecutiveClicks++
+
+      console.log(totalConsecutiveClicks)
+
+      if (!TIPS_DISABLED && totalConsecutiveClicks >= 10) {
+        console.log('tip you can press the date to open a calender view');
+        totalConsecutiveClicks = 0;
+      }
+    }
+
     const currentDate = ref(new Date());
 
     const formatDate = (date) => {
@@ -10,6 +26,13 @@ export default {
     };
 
     const updateDate = (modifier) => {
+      const updatedDate = currentDate.value.getDate() + modifier;
+
+      if (updatedDate > TODAYS_DATE.getDate()) {
+        console.log('Exiting early we can\'t move to the next day')
+        return;
+      }
+
       currentDate.value = new Date(currentDate.value.setDate(currentDate.value.getDate() + modifier));
       emit("dateChanged", currentDate.value);
     };
@@ -18,11 +41,14 @@ export default {
       formattedDate: computed(() => formatDate(currentDate.value)),
       prevDate: () => updateDate(-1),
       nextDate: () => updateDate(1),
+      increment
     };
   },
   template: `<div class="d-flex align-items-center justify-content-center gap-3">
-    <button class="btn btn-primary" @click="prevDate">&lt;</button>
+    <button class="btn btn-primary" @click="prevDate(); increment();">&lt;</button>
     <div class="px-4 py-2 border rounded bg-light">{{ formattedDate }}</div>
     <button class="btn btn-primary" @click="nextDate">&gt;</button>
+
+    <v-date-picker hidden></v-date-picker>
   </div>`
 }
