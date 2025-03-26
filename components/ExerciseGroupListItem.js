bb706d324f1983d8207675ddea02b77f315378a2
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2025-03-26 12:16:30 -0500

    tweak(ExerciseGroupListItem): make the component look a little better

--- components/ExerciseGroupListItem.js
+++ components/ExerciseGroupListItem.js
@@ -1,5 +1,5 @@
 export default {
-  emits: ["delete-exercise"],
+  emits: ["delete-exercise", "edit-exercise"],
   props: {
     exercise: {
       type: Object,
@@ -8,14 +8,27 @@ export default {
   },
   methods: {
     deleteExercise() {
-      this.$emit('delete-exercise', this.exercise);
+      this.$emit("delete-exercise", this.exercise);
+    },
+    editExercise() {
+      this.$emit("edit-exercise", this.exercise);
     },
   },
   template: `
-    <div class="list-group-item d-flex justify-content-between align-items-center">
-      <p>{{ exercise.name }}</p>
-      <span>Sets: {{ exercise.sets }} | Reps: {{ exercise.reps }} | Weight: {{ exercise.weight }} | Time/Set: {{ exercise.timePerSet }}</span>
-      <button @click="deleteExercise" class="btn btn-danger btn-sm ml-2">Delete</button>
+    <div class="list-group-item p-3 border rounded shadow-sm bg-light">
+      <div class="d-flex justify-content-between align-items-center">
+        <h5 class="mb-0">{{ exercise.name }}</h5>
+        <div>
+          <button @click="editExercise" class="btn btn-outline-primary btn-sm me-2">Edit</button>
+          <button @click="deleteExercise" class="btn btn-outline-danger btn-sm">Delete</button>
+        </div>
+      </div>
+      <p class="mt-2 mb-0 text-muted">
+        <strong>Sets:</strong> {{ exercise.sets }} &nbsp; | &nbsp; 
+        <strong>Reps:</strong> {{ exercise.reps }} &nbsp; | &nbsp; 
+        <strong>Weight:</strong> {{ exercise.weight }} &nbsp;
+        <!-- <strong>Time/Set:</strong> <span v-if="exercise.timePerSet">{{ exercise.timePerSet }}</span> -->
+      </p>
     </div>
   `,
-}
+};
